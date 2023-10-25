e41cd8f9fa9ef9da65bf279b421749b20e34bc51
anirudhv98 <vanirudh98@gmail.com>
2023-10-24 23:18:57 -0700

    Implement 1 round of Rock, Paper& Scissors

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("BOILERPLATE");
+function getComputerChoice()
+{
+    var array = ["rock","paper","scissor"];
+    var random = Math.floor((Math.random() * 3));
+    return array[random];
+}
+
+function playRound(playerSelection , computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    if((playerSelection=="rock" && computerSelection=="scissor")
+    || (playerSelection=="paper" && computerSelection=="rock") 
+    || (playerSelection=="scissor" && computerSelection=="paper"))
+    {
+        return `You Won! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if(playerSelection==computerSelection)
+    {
+        return `Scores tied, both chose ${playerSelection}`;
+    }
+    else 
+    {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
