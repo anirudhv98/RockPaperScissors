5951c7621b4bd9c2cdc36f15cbbc1d08f1f173b4
anirudhv98 <vanirudh98@gmail.com>
2023-11-28 13:10:29 -0700

    Refined logic to support UI

--- script.js
+++ script.js
@@ -1,71 +1,85 @@
-// function getComputerChoice()
-// {
-//     let choice = Math.floor(Math.random() * 3);
-//     if(choice==0)
-//     {
-//         return "rock";
-//     }
-//     else if(choice == 1)
-//     {
-//         return "paper";
-//     }
-//     else
-//     {
-//         return "scissors";
-//     }
-// }
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice == 0) {
+    return "rock";
+  } else if (choice == 1) {
+    return "paper";
+} else {
+    return "scissors";
+}
+}
 
-// function play(computer, player =  "rock")
-// {
-//     console.log(player);
-//     console.log(computer);
-//     if((computer == "rock" && player == "rock") || (computer == "scissors" && player == "scissors") || (computer == "paper" && player == "paper"))
-//     {
-//         alert("This round is a draw");
-//         return 0;
-//     }
-//     else if ((player == "rock" && computer == "scissors") || 
-//     (player == "scissors" && computer == "paper") || 
-//     (player == "paper" &&  computer == "rock"))
-//     {
-//         alert(`You Won! ${player} beats ${computer}`);
-//         return 1;
-//     }
-//     else 
-//     {
-//         alert(`You Lose! ${computer} beats ${player}`);
-//         return -1;
-//     }
-// }
+let scoreboard = document.querySelector(".scoreboard");
+let roundResult = document.querySelector(".outer");
+let inner = document.querySelector(".inner");
+function play(computer, player) {
+  console.log(player);
+  console.log(computer);
+  if (
+    (computer == "rock" && player == "rock") ||
+    (computer == "scissors" && player == "scissors") ||
+    (computer == "paper" && player == "paper")
+  ) {
+    roundResult.textContent = "This round is a draw";
+    return 0;
+  } else if (
+    (player == "rock" && computer == "scissors") ||
+    (player == "scissors" && computer == "paper") ||
+    (player == "paper" && computer == "rock")
+  ) {
+    roundResult.textContent = `You Won! ${player} beats ${computer}`;
+    return 1;
+  } else {
+    roundResult.textContent = `You Lose! ${computer} beats ${player}`;
+    return -1;
+  }
+}
 
-// let computerScore =0;
-// let playerScore =0;
+let playAgain = document.querySelector("#reset");
+playAgain.addEventListener("click", reset);
+let played = false;
 
-// for(let i=0;i<5;i++)
-// {
-//     let computer = getComputerChoice();
-//     let player = prompt("Enter your choice").toLowerCase();
-//     let result = play(computer,player);
-//     if(result==1)
-//     {
-//         playerScore+=1;
-//     }
-//     else if(result == -1)
-//     {
-//         computerScore +=1;
-//     }
-// }
+function reset() {
+  computerScore = 0;
+  playerScore = 0;
+  scoreboard.textContent = `Player ${playerScore} : Computer ${computerScore}`;
+  played = false;
+  playAgain.style.visibility = played ? "visible" : "hidden";
+  roundResult.textContent = "";
+  inner.textContent="";
+}
 
+function round(player) {
+  if (!played) {
+    let computer = getComputerChoice();
+    let result = play(computer, player);
+    if (result == 1) {
+      playerScore += 1;
+    } else if (result == -1) {
+      computerScore += 1;
+    }
+    scoreboard.textContent = `Player ${playerScore} : Computer ${computerScore}`;
+  }
+  if (playerScore == 5 || computerScore == 5) {
+    played = true;
+    if (computerScore > playerScore) {
+      roundResult.textContent = `Computer won the series ${computerScore} - ${playerScore}`;
+    } else if (playerScore > computerScore) {
+      roundResult.textContent = `Player won the series ${playerScore} - ${computerScore}`;
+    } else {
+      roundResult.textContent = "Series is a draw";
+    }
+    inner.textContent = "Click on the Button below to play again";
+    playAgain.style.visibility = played ? "visible" : "hidden";
+  }
+}
 
-// if(computerScore>playerScore)
-// {
-//     alert(`Computer won the series ${computerScore} - ${playerScore}`);
-// }
-// else if(playerScore>computerScore)
-// {
-//     alert(`Player won the series ${playerScore} - ${computerScore}`);
-// }
-// else 
-// {
-//     alert("Series is a draw");
-// }
+let computerScore = 0;
+let playerScore = 0;
+let buttons = document.querySelectorAll(".choice");
+
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    round(button.value);
+  });
+});
